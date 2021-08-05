572b79ca73bd31a4fdbc0ff0a94d7749025af0de
Muhammad Fauzan <fncolon@pm.me>
2021-08-06 03:58:10 +0700

    fix(test-hook): change beforeEach to before

--- test/home.spec.js
+++ test/home.spec.js
@@ -3,6 +3,11 @@ let assert = require("chai").assert;
 const { fetchText } = require("../lib/test/fetcher");
 
 describe("/", function () {
+  it('Match "Welcome" inside content', async function () {
+    const content = await fetchText("http://localhost:3000");
+    assert.match(content, /Welcome/g);
+  });
+
   it('Match "Aksara" inside the content', async function () {
     const content = await fetchText("http://localhost:3000");
     assert.match(content, /Aksara/g);
